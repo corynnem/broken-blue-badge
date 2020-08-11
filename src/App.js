a5194b68ae042b711f429a8eac03494b1a562e67
corynnem <corynnem@users.noreply.github.com>
2020-08-11 13:19:33 -0400

    first push

---
+++ src/App.js
@@ -0,0 +1,66 @@
+import React, {useState, useEffect} from 'react';
+import Typed from 'react-typed';
+import './App.css';
+
+function App() {
+
+  const [affirmations, setAffirmations] = useState()
+
+  const affirmationStyles = {
+    fontSize: '5vh',
+  }
+  
+
+  const fetchResults = (e) => {
+    // e.preventDefault();
+    const url = 'https://quotes.rest/qod/inspire'
+    const APIKey = 'Cm_985948'
+
+    fetch(url, {
+      method: 'GET',
+      headers: {
+        'Accept': 'application/json',
+        'X-TheySaidSo-Api-Secret': APIKey,
+      },
+    
+    })
+    .then((data) => data.json())
+    .then(data => setAffirmations(data.contents.quotes[0].quote))
+  }
+ 
+    useEffect(() => {
+      fetchResults()
+    }, []);
+
+
+  
+
+  return (
+    <div className="App">
+      <br>
+      </br>
+       <Typed
+          style={affirmationStyles}
+          strings={['Good Morning Mandy!!', 'Are you inspired?']}
+          typeSpeed={100}
+          />
+          <br>
+          </br>
+          <br>
+          </br>
+    
+        <h1 style={{
+        fontSize: '3vh', 
+        padding: '10%', 
+        backgroundColor: 'rgba(25, 25, 25, 0.7)', 
+        width: '75vw', 
+        marginLeft: '2%',
+        color: 'white',
+        fontFamily: 'Quicksand, sans-serif',
+        }} >{affirmations}</h1>
+        
+    </div>
+  );
+}
+
+export default App;
